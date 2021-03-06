3ebc337604457c3f18704b5d74dd121fc1f732ab
kimutai-codes <allankimutai@gmail.com>
2021-03-06 12:51:44 +0300

    state hook

--- src/app.js
+++ src/app.js
@@ -2,23 +2,24 @@ import React from "react";
 import styled from "styled-components";
 import "./app.css";
 import Forecast from "./components/forecast";
+const Footer = styled.footer`
+  background-color: grey;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  height: 40px;
+`;
+
 const App = () => {
-  const Mguu = styled.footer`
-    background-color: grey;
-    position: fixed;
-    bottom: 0;
-    width: 100%;
-    height: 40px;
-  `;
   return (
     <div>
       <main>
         <h1>WEATHER APP</h1>
         <Forecast />
       </main>
-      <Mguu>
+      <Footer>
         Page created by <a href="##"> @kimutai</a>
-      </Mguu>
+      </Footer>
     </div>
   );
 };
--- src/components/forecast.js
+++ src/components/forecast.js
@@ -1,54 +1,49 @@
 //using the state hook
 import react, { useState } from "react";
 import styled from "styled-components";
+const Input = styled.input`
+  border: none;
+  padding: 6px;
+  border-radius: 5px;
+  font-weight: bold;
+  color: black;
+`;
+const Button = styled.button`
+  display: inline-block;
+  margin: 10px;
+  background-color: darkcyan;
+  border: none;
+  border-radius: 5px;
+  padding: 5px;
+`;
 
 const Forecast = () => {
-  const Input = styled.input`
-    border: none;
-    padding: 6px;
-    border-radius: 5px;
-    font-weight: bold;
-    color: black;
-  `;
-  const Button = styled.button`
-    display: inline-block;
-    margin: 10px;
-    background-color: darkcyan;
-    border: none;
-    border-radius: 5px;
-    padding: 5px;
-  `;
-  const Search = styled.form`
-    // display: flex;
-  `;
+  const APIKEY = "7e23a3dc74fc86b89fba3917d84b6ebf";
   //react state hook
   const [city, setCity] = useState("");
-
-  const handleChange = (e) => {
-    //the reason I get frustrated is because I code without understanding and I'm too proud and lazy to search
-    e.preventDefault();
-    let name = e.target.name;
-    let value = e.target.value;
-    setCity("e.target.value");
-    console.log(value);
-  };
   //I need to learn about forms and then how apis work
   //also need to refresh on destructuring and the spread operator
-
+  //  //the reason I get frustrated is because I code without understanding and I'm too proud and lazy to search
+  const handleChange = (e) => {
+    setCity(e.target.value);
+    console.log(" change:", city);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(" submit:", city);
+  };
   return (
-    <div>
+    <form onSubmit={handleSubmit}>
       {/* use onChange for the most part and use onSubmit onlwhen actually submitting the form */}
-      <Search>
-        <Input
-          type="text"
-          name="search"
-          placeholder="search city"
-          onChange={(e) => handleChange(e)}
-        ></Input>
-        <br />
-        <Button type="submit">Get Forecast</Button>
-      </Search>
-    </div>
+      <Input
+        placeholder="search city"
+        required
+        value={city}
+        onChange={handleChange}
+      />
+      <br />
+      <Button>Get Forecast</Button>
+    </form>
   );
 };
 
